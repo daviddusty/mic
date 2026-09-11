48c927b214a597a3d80b42decc02a70bceb5f700
David <40916787+daviddusty@users.noreply.github.com>
2026-09-11 08:48:26 +0200

    Update resource.h

--- micmute_code_r38/key_hook/resource.h
+++ micmute_code_r38/key_hook/resource.h
@@ -1,5 +1,5 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
+// Microsoft Visual C++ generated include file..
 // Used by key_hook.rc
 
 // Next default values for new objects
